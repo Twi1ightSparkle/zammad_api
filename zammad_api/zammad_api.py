8dcb7d5f2cc537230ca3ace18f8a265653f5ef36
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-09-08 13:40:47 +0100

    many improvements

--- zammad_api/zammad_api.py
+++ zammad_api/zammad_api.py
@@ -19,32 +19,32 @@ def req(method, target, token, data=None):
 
     # Check method valid
     if method not in ['GET', 'PUT', 'POST', 'DELETE']:
-        return {'error': ['rest_api.req', f'Invalid method {method}']}
+        raise AttributeError('Invalid method %s' % method)
     
     # Check that data supplied with PUT and POST
     if (method == 'PUT' or method == 'POST') and not data:
-        return {'error': ['rest_api.req', f'data must be supplied when {method} is used']}
+        raise TypeError('JSON must be supplied when %s is used' % method)
 
     # Build header and data
     if data:
-        headers = {'Authorization': f'Token token={token}', 'Content-Type': 'application/json'}
+        headers = {'Authorization': 'Token token=%s' % token, 'Content-Type': 'application/json'}
         try:
             data = json.loads(data)
         except json.decoder.JSONDecodeError:
-            return {'error': ['rest_api.req', 'Supplied data is not valid JSON']}
+            raise ValueError('Invalid JSON supplied')
     else:
-        headers = {'Authorization': f'Token token={token}'}
+        headers = {'Authorization': 'Token token=%s' % token}
 
     # Try connecting
     try:
         response = requests.request(method=method, url=target, headers=headers, data=data)
 
     except Exception as error:
-        return {'error': ['rest_api.req', f'Error connecting to {target}. Error message: {error}']}
+        return {'error_target': target, 'error_message': error, 'status_code': 1},
 
     # If not 200 or 201
     if not response.status_code == 200 and not response.status_code == 201:
-        return {'error': ['rest_api.req', f'Error connecting to {target}. Status code {response.status_code}']}
+        return {'error_target': target, 'error_message': 'Unknown error', 'status_code': response.status_code}
     
     # Try and decode json
     try:
@@ -52,7 +52,7 @@ def req(method, target, token, data=None):
 
     # If converting to json fails
     except json.decoder.JSONDecodeError as error:
-        return {'error': ['rest_api.req', f'Error decoding JSON {error}']}
+        raise Exception('Error deciding JSON returned from API')
 
 
 # Classes
@@ -63,29 +63,23 @@ class ZammadApi:
     api_endpoint = ''
     description = ''
     
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        self.target = f'{target}/api/v1/'
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        self.target = '%s/api/v1/' % target
         self.api_key = api_key
         self.json_data = json.dumps(json_data)
-        self.obj_id = str(obj_id)
-        # self.api_endpoint = self.target + self.api_endpoint
+        self.object_type = object_type
+        self.object_id = str(object_id)
         if filter_string:
             self.filter_string = str(filter_string)
         else:
             self.filter_string = None
         
-        if self.obj_id:
-            self.api_endpoint = self.target + self.api_endpoint + '/' + str(self.obj_id)
+        if self.object_type and self.object_id:
+            self.api_endpoint = self.target + self.api_endpoint + '?object=' + self.object_type + '&o_id=' + self.object_id
+        elif self.object_id:
+            self.api_endpoint = self.target + self.api_endpoint + '/' + str(self.object_id)
         else:
             self.api_endpoint = self.target + self.api_endpoint
-    
-    # Disallow direct instantiation of this parent class
-    # TODO This isn't working for some reason....
-    # def __new__(cls, *args, **kwargs):
-    #     print(cls)
-    #     if cls is ZammadApi:
-    #         raise TypeError("The parent class ZammadApi can not be directly instantiated")
-    #     return object.__new__(cls, *args, **kwargs)
 
 
     def action(self, method):
@@ -102,15 +96,8 @@ class ZammadApi:
             self.result = req(method, self.api_endpoint, self.api_key, self.json_data)
         else:
             self.result = req(method, self.api_endpoint, self.api_key)
-
         
-        if 'error' in self.result:
-            err_func = self.result.get('error')[0]
-            err_msg = self.result.get('error')[1]
-            print(f'API error from function {err_func}\nError message: {err_msg}')
-            exit(1)
-        else:
-            return self.result
+        return self.result
 
 
     # --clone
@@ -119,7 +106,7 @@ class ZammadApi:
         pass
     
     # --list
-    def list_obj(self):
+    def list_objects(self):
         self.results = self.objs = self.action('GET')
         if self.filter_string:
             self.filter_hits = []
@@ -131,7 +118,7 @@ class ZammadApi:
             if len(self.filter_hits) > 0:
                 return self.filter_hits
             else:
-                return {'error': f'No {self.description}s containing {self.filter_string} was found'}
+                return {'error': 'No %ss containing %s was found' % (self.description, self.filter_string)}
 
         else:
             return self.results
@@ -139,10 +126,10 @@ class ZammadApi:
     # --get
     def get(self):
         self.objs = self.action('GET')
-        for self.obj in self.objs:
-            if self.obj.get('obj_id') == self.obj_id:
-                return self.obj
-        return {'error': f'{self.description} with obj_id {self.obj_id} not found'}
+        for self.object in self.objs:
+            if self.object.get('object_id') == self.object_id:
+                return self.object
+        return {'error': '%s with object_id %s not found' % (self.description, self.object_id)}
     
     # --new
     def new(self):
@@ -150,12 +137,6 @@ class ZammadApi:
     
     # --update
     def update(self):
-        # Print old and new data
-        # print('You are about to change\n')
-        # misc.pp_json(self.get())
-        # print('\nto\n')
-        # misc.pp_json(self.json_data)
-
         # Ask for confirmation
         self.confirmation = input('\nDo you want to continue? [y/N] ')
         if self.confirmation.lower() == 'y':
@@ -163,10 +144,6 @@ class ZammadApi:
 
     # --delete
     def delete(self):
-        # Print old and new data
-        # print('You are about to delete:\n')
-        # misc.pp_json(self.json_data)
-
         # Ask for confirmation
         self.confirmation = input('\nDo you want to continue? [y/N] ')
         if self.confirmation.lower() == 'y':
@@ -178,10 +155,10 @@ class Tag(ZammadApi):
     api_endpoint = 'tag_list'
     description = 'tag'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
-    def list_obj(self):
+    def list_objects(self):
         if self.filter_string:
             self.api_endpoint = self.api_endpoint.replace('tag_list', 'tag_search') + '?term=' + self.filter_string
 
@@ -190,7 +167,20 @@ class Tag(ZammadApi):
         if len(self.results) > 0:
             return self.results
         else:
-            return {'error': f'No {self.description}s containing {self.filter_string} was found'}
+            return {'error': 'No %ss containing %s was found' % (self.description, self.filter_string)}
+
+
+class TicketTags(ZammadApi):
+
+    api_endpoint = 'tags'
+    description = 'ticket'
+
+    def __init__(self, target, api_key, filter_string=None, object_type='Ticket', object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
+
+    def get(self):
+        """Get all tags for a ticket"""
+        return self.action('GET')
 
 
 class EmailFilter(ZammadApi):
@@ -198,8 +188,8 @@ class EmailFilter(ZammadApi):
     api_endpoint = 'postmaster_filters'
     description = 'email filter_string'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class EmailSignature(ZammadApi):
@@ -207,8 +197,8 @@ class EmailSignature(ZammadApi):
     api_endpoint = 'signatures'
     description = 'email signature'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class Group(ZammadApi):
@@ -216,8 +206,8 @@ class Group(ZammadApi):
     api_endpoint = 'groups'
     description = 'group'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class KnowledgeBase(ZammadApi):
@@ -225,8 +215,8 @@ class KnowledgeBase(ZammadApi):
     api_endpoint = ''
     description = 'knowledge base'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class Macro(ZammadApi):
@@ -234,19 +224,12 @@ class Macro(ZammadApi):
     api_endpoint = 'macros'
     description = 'macro'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
     def get(self):
-        self.api_endpoint = self.api_endpoint + '/#' + self.obj_id
-        print(self.api_endpoint)
+        self.api_endpoint = self.api_endpoint + '/#' + self.object_id
         return self.action('GET')
-
-        # self.objs = self.action('GET')
-        # for self.obj in self.objs:
-        #     if self.obj.get('obj_id') == self.obj_id:
-        #         return self.obj
-        # return {'error': f'{self.description} with obj_id {self.obj_id} not found'}
 
 
 class Organization(ZammadApi):
@@ -254,10 +237,10 @@ class Organization(ZammadApi):
     api_endpoint = 'organizations'
     description = 'organization'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
-    def list_obj(self):
+    def list_objects(self):
         if self.filter_string:
             self.api_endpoint = self.api_endpoint + '/search?query=' + self.filter_string + '&limit=999999999999999'
 
@@ -266,7 +249,7 @@ class Organization(ZammadApi):
         if len(self.results) > 0:
             return self.results
         else:
-            return {'error': f'No {self.description}s containing {self.filter_string} was found'}
+            return {'error': 'No %ss containing %s was found' % (self.description, self.filter_string)}
 
 
 class Overview(ZammadApi):
@@ -274,19 +257,17 @@ class Overview(ZammadApi):
     api_endpoint = 'overviews'
     description = 'overview'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
     
-
-
 
 class Role(ZammadApi):
 
     api_endpoint = 'roles'
     description = 'role'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class Ticket(ZammadApi):
@@ -294,14 +275,11 @@ class Ticket(ZammadApi):
     api_endpoint = 'tickets'
     description = 'ticket'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
     
     def get(self):
-        self.ticket = self.action('GET')
-        if str(self.ticket['id']) == self.obj_id:
-            return self.ticket
-        return {'error': f'{self.description} with obj_id {self.obj_id} not found'}
+        return self.action('GET')
 
 
 class Trigger(ZammadApi):
@@ -309,8 +287,8 @@ class Trigger(ZammadApi):
     api_endpoint = 'triggers'
     description = 'trigger'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
 
 class User(ZammadApi):
@@ -318,16 +296,13 @@ class User(ZammadApi):
     api_endpoint = 'users'
     description = 'user'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
 
     def get(self):
-        self.user = self.action('GET')
-        if str(self.user['id']) == self.obj_id:
-            return self.user
-        return {'error': f'{self.description} with obj_id {self.obj_id} not found'}
+        return self.action('GET')
 
-    def list_obj(self):
+    def list_objects(self):
         if self.filter_string:
             self.api_endpoint = self.api_endpoint + '/search?query=' + self.filter_string
             self.results = self.action('GET')
@@ -346,7 +321,7 @@ class User(ZammadApi):
         if len(self.results) > 0:
             return self.results
         else:
-            return {'error': f'No {self.description}s containing {self.filter_string} was found'}
+            return {'error': 'No %ss containing %s was found' % (self.description, self.filter_string)}
 
 
 class Collection(ZammadApi):
@@ -354,5 +329,5 @@ class Collection(ZammadApi):
     api_endpoint = ''
     description = 'collection'
 
-    def __init__(self, target, api_key, filter_string=None, obj_id=None, json_data=None):
-        super().__init__(target, api_key, filter_string, obj_id, json_data)
+    def __init__(self, target, api_key, filter_string=None, object_type=None, object_id=None, json_data=None):
+        super().__init__(target, api_key, filter_string, object_type, object_id, json_data)
